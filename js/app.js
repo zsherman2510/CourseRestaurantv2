c0adcfb3361aec46aa27eb98b9df9e171afd6b72
zavion sherman <zsherman2510@hotmail.com>
2021-06-14 23:37:36 -0700

    finished version before animations

---
+++ js/app.js
@@ -0,0 +1,8 @@
+var hamburger = document.getElementById("hamburger");
+var menuMobile = document.getElementById("menu_mobile");
+
+hamburger.addEventListener("click", function () {
+  menuMobile.classList.contains("menu_show")
+    ? menuMobile.classList.remove("menu_show")
+    : menuMobile.classList.add("menu_show");
+});
